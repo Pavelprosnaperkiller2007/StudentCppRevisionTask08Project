7dc9ff64cc4e17b43c4eba39fb0430d330e031b8
Vladimr Kondratsenko <vovanych1981@gmail.com>
2023-05-04 17:11:22 +0300

    addd

--- StudentCppRevisionTask08Project/task03.cpp
+++ StudentCppRevisionTask08Project/task03.cpp
@@ -21,5 +21,19 @@
  */
 
 int task03(int a, int b, int c, int d) {
-	return 0;
+
+	if (a < b && a < c && a < d) {
+		return a;
+	} 
+	else if (b < a && b < c && b < d) {
+		return b;
+	}
+
+	else if (c < a && c < b && c < d) {
+		return c;
+	}
+
+	else {
+		return d;
+	}
 }
